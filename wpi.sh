a1277fb4768ea7282ed190ac2b10cb19d3525171
DimaMinka <cdk@cdk.co.il>
2020-05-03 21:59:37 +0300

    fix--wpi--refactor the dev and cloud loading

--- wpi.sh
+++ wpi.sh
@@ -4,9 +4,10 @@
 # by Dima Minka (https://dima.mk)
 # https://cloud.wpi.pw
 
-# Get cloud source scripts from github
-CLOUD_SOURCE="<(curl -s https://raw.githubusercontent.com/wpi-pw/cloud/master/bin/source.sh)"
-# Replace the cloud source path to local on dev flag
-[[ "$1" == "dev" ]] && CLOUD_SOURCE="${PWD}/bin/source.sh"
-# Load source scripts
-source $CLOUD_SOURCE
+if [[ "$1" == "dev" ]]; then
+  # Load local source scripts on dev flag
+  source ${PWD}/bin/source.sh
+else
+  # Load cloud source scripts from github
+  source <(curl -s https://raw.githubusercontent.com/wpi-pw/cloud/master/bin/source.sh)
+fi

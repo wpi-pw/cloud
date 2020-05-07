89f4a5927f7c82c2446c0728958590a0e158d881
DimaMinka <cdk@cdk.co.il>
2020-05-07 09:09:18 +0300

    feat--help--reading features done
    * cloud reading
    * local reading
    * load help features from source.sh
    * app help adding
    * support for -h --help flag

---
+++ bin/app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# WPI Cloud - app
+# by Dima Minka (https://dima.mk)
+# https://cloud.wpi.pw
+
+echo "app command development in progress"
--- bin/source.sh
+++ bin/source.sh
@@ -13,9 +13,27 @@ readonly NC='\033[0m'     # no color
 
 # Check for a registered commands
 case "$1" in
-  init | help);;
+  app | init | help);;
   *)  printf "${RED}Warning:${NC} '%s' is not a registered wpi command. See 'wpi help'. \n" "$1" && exit 1 ;;
 esac
+
+# Get and prepare args and flags for help docs reading
+for i in "$@"; do
+  case "$i" in
+    --help | -h)  HELP=true;;                   # Enable help docs
+    --local | -l) LOCAL=true;;                  # Enable local loading
+    help)         HELP=true; HELP_PATH+="/$i";; # Enable main help docs
+    *)            HELP_PATH+="/$i";;            # Build help file path
+  esac
+done
+
+# Read help docs if help command running
+if [[ -n "$HELP" ]]; then
+  [[ -z "$LOCAL" ]] && CAT_PATH="cat <(curl -s -L wpi.pw/help$HELP_PATH.yml)" # Read docs from cloud
+  [[ -n "$LOCAL" ]] && CAT_PATH="cat ${PWD}/help$HELP_PATH.yml"               # Read docs from local
+  eval $CAT_PATH | egrep -v "^\s*(#)" | more -d
+  exit 1
+fi
 
 # Check if the function exists (bash specific)
 if declare -f "$1" > /dev/null

330a7017c87d7a8877a351d51dd08d633f667427
Dima Minka <cdk@cdk.co.il>
2020-06-29 17:57:44 +0300

    feat--release.sh--support for loop of symlinks

--- bin/themes/release.sh
+++ bin/themes/release.sh
@@ -15,19 +15,26 @@ done
 source <(curl -s https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-source.sh)
 
 cur_env=$(cur_env)
-# Copy current release of theme
-if [[ "$(wpi_yq themes.parent.symlink.env)" == "$cur_env" ]]; then
-  app_dir=${PWD}
-  app_content=$( wpi_yq "env.$cur_env.app_content" )
-  theme_name=$(echo $( wpi_yq "themes.parent.name" ) | cut -d"/" -f2)
-  theme_path="${app_dir%/}${app_content%/}/themes/$theme_name"
-  theme_symlink=$( wpi_yq 'themes.parent.symlink.path' )
-  if [[ -d "$theme_symlink" ]]; then
-    printf "${GRN}====================================================${NC}\n"
-    printf "${GRN}Copy corrent release of $theme_name                 ${NC}\n"
-    printf "${GRN}====================================================${NC}\n"
-    rsync -rvq --exclude=node_modules "$theme_symlink/" "$theme_path-tmp"
-    rm -rf $theme_path
-    mv "$theme_path-tmp" $theme_path
+
+# Create array of theme symlinks and loop
+mapfile -t symlinks < <( wpi_yq 'themes.parent.symlink.[*].env' )
+# Get all symlink env and run the script for the current
+for i in "${!symlinks[@]}"
+do
+  # Copy current release of theme
+  if [[ "$(wpi_yq themes.parent.symlink.[$i].env)" == "$cur_env" ]]; then
+    app_dir=${PWD}
+    app_content=$( wpi_yq "env.$cur_env.app_content" )
+    theme_name=$(echo $( wpi_yq "themes.parent.name" ) | cut -d"/" -f2)
+    theme_path="${app_dir%/}${app_content%/}/themes/$theme_name"
+    theme_symlink=$( wpi_yq "themes.parent.symlink.[$i].path" )
+    if [[ -d "$theme_symlink" ]]; then
+      printf "${GRN}====================================================${NC}\n"
+      printf "${GRN}Copy current release of $theme_name                 ${NC}\n"
+      printf "${GRN}====================================================${NC}\n"
+      rsync -rvq --exclude=node_modules "$theme_symlink/" "$theme_path-tmp"
+      rm -rf $theme_path
+      mv "$theme_path-tmp" $theme_path
+    fi
   fi
-fi
+done

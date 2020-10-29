2aa31d2dcc358512dc8ed93c3ed15260c0b7b0ae
Dima Minka <cdk@cdk.co.il>
2020-10-29 16:56:56 +0200

    feat--wpirock.sh--installer init

---
+++ bin/wpirock.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# WPI Cloud - wpirock
+# by Dima Minka (https://dima.mk)
+# https://cloud.wpi.pw
+
+# Define colors
+readonly RED='\033[0;31m' # error
+readonly GRN='\033[0;32m' # success
+readonly BLU='\033[0;34m' # task
+readonly BRN='\033[0;33m' # headline
+readonly NC='\033[0m'     # no color
+
+printf "%s${GRN}Running:${NC} Download and run WpiRock\n"
